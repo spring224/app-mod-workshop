a2b59c9ebe8cad6bdf7da795959451be4d99ebe8
giorgio balzarotti <gbalzarotti@gmail.com>
2025-03-20 15:44:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.154.109.51';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = 'Marzo2025!';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
@@ -14,3 +14,4 @@ try {
 
 session_start();
 ?>
+gcloud run deploy --source .
